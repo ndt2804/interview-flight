a85a3df41570610bbbe6dd90de4b9feb9b97ade6
ndt2804 <nduytan.uit.a3@gmail.com>
2025-01-05 23:03:33 +0700

    ci: initial commit

---
+++ testflights/Models/Flight.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testflights.Models
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string Airline { get; set; }
+        public DateTime DepartureTime { get; set; }
+
+        public string OriginAirport { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public string DestinationAirport{ get; set; }
+    }
+}
